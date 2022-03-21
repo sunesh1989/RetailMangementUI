a6cd201c37598398f6d2761af5faa9581ecad880
sunesh1989 <suneshkumarp@gmail.com>
2022-03-21 16:16:48 +0530

    removed unwanted spaces

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,23 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-
-
   constructor(private httpClient: HttpClient) { }
-  
   getProducts():Observable<any>{
-
-    return this.httpClient.get("https://localhost:44330/api/product")
-
+    //As of now top 100 records will display but its configurable
+    return this.httpClient.get("https://localhost:44330/api/product/100/0")
   }
   getProductDetails(id:string):Observable<any>{
-
     return this.httpClient.get("https://localhost:44330/api/product/"+id)
-
   }
 }
